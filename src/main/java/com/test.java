f911f1bd58156f711ecb99a0aed8e03128e18092
Xin Li <xin.li@ui.com>
2022-03-16 08:59:08 +0800

    dev分支提交2

--- src/main/java/com/test.java
+++ src/main/java/com/test.java
@@ -3,7 +3,8 @@ package com;
 public class test {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
         System.out.println("dev");
+        System.out.println("dev");
+
     }
 }
